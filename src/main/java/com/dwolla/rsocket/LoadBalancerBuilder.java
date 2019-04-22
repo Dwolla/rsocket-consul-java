9cda82905f1fbb10c1efebf8e12aab131138c430
Jared Dellitt <jared@dwolla.com>
2019-04-22 14:34:35 -0500

    Set timeout to 90 seconds for http requests to consul

--- src/main/java/com/dwolla/rsocket/LoadBalancerBuilder.java
+++ src/main/java/com/dwolla/rsocket/LoadBalancerBuilder.java
@@ -2,11 +2,16 @@ package com.dwolla.rsocket;
 
 import com.dwolla.rsocket.consul.AsyncHttpClientImpl;
 import io.rsocket.client.LoadBalancedRSocketMono;
+import org.asynchttpclient.AsyncHttpClient;
+
+import java.time.Duration;
 
 import static org.asynchttpclient.Dsl.asyncHttpClient;
+import static org.asynchttpclient.Dsl.config;
 
 public class LoadBalancerBuilder {
   private String consulHost = "http://localhost:8500";
+  private int requestTimeout = (int) Duration.ofSeconds(90).toMillis();
 
   public LoadBalancerBuilder withConsul(String consul) {
     this.consulHost = consul;
@@ -14,9 +19,13 @@ public class LoadBalancerBuilder {
   }
 
   public LoadBalancedRSocketMono build(String service) {
+    AsyncHttpClient asyncHttpClient =
+        asyncHttpClient(
+            config().setRequestTimeout(requestTimeout).setReadTimeout(requestTimeout).build());
+
     ConsulRSocketLoadBalancerFactory factory =
         new ConsulRSocketLoadBalancerFactory(
-            new ConsulHealthStream(new AsyncHttpClientImpl(asyncHttpClient()), consulHost));
+            new ConsulHealthStream(new AsyncHttpClientImpl(asyncHttpClient), consulHost));
 
     return factory.create(service);
   }
